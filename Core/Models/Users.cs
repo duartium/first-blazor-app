ede02d4fb86b7290a699cf3cb7c88082eeb3c73c
Byron Duarte <byronduarte95@gmail.com>
2024-11-08 12:58:59 -0500

    :100:  feat: passing components into components; card list of users; installing daysi UI; additional attributes

---
+++ Core/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿namespace first_blazor_app.Core.Models.Users;
+
+public class User
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Profile { get; set; }
+
+	public User(string id, 
+		string userName, 
+		string profile)
+	{
+		Id = id;
+		UserName = userName;
+		Profile = profile;
+	}
+}
